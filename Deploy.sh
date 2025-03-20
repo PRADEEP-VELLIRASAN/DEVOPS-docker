4a575262ad0645bc188c56ada0f025d95793dd84
Sabarivasan <144119741+Sabari-Vasan-SM@users.noreply.github.com>
2025-03-20 11:30:04 +0530

    Update Deploy.sh

--- Deploy.sh
+++ Deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set Docker image name, tag, and container name
-IMAGE_NAME="santhosh1291245/docker"
+IMAGE_NAME="sabarivasan1239/docker"
 TAG="latest"
 CONTAINER_NAME="my-container"
 
